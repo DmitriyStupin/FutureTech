7de1f4a85b26a8573dd5996c6c1267ed0ac19bb6
Dmitry Stupin <dm.stup@mail.ru>
2025-01-11 13:26:12 +0700

    a custom select has been completed, added to the contacts page, and minor layout flaws have been fixed

--- scripts/Select.js
+++ scripts/Select.js
@@ -67,7 +67,7 @@ class Select extends BaseComponent {
       this.buttonElement.classList.toggle(this.stateClasses.isExpanded, isExpanded)
       this.buttonElement.setAttribute(this.stateAttributes.ariaExpanded, isExpanded)
       this.buttonElement.setAttribute(
-        this.stateAttributes.ariaActiveDescendant, 
+        this.stateAttributes.ariaActiveDescendant,
         this.optionElements[currentOptionIndex].id
       )
     }
@@ -97,6 +97,14 @@ class Select extends BaseComponent {
     this.state.isExpanded = !this.state.isExpanded;
   }
 
+  expand() {
+    this.state.isExpanded = true
+  }
+
+  collapse() {
+    this.state.isExpanded = false
+  }
+
   fixDropdownPosition() {
     const viewportWidth = document.documentElement.clientWidth
     const halfViewportX = viewportWidth / 2
@@ -120,17 +128,112 @@ class Select extends BaseComponent {
     this.buttonElement.tabIndex = isMobileDevice ? -1 : 0
   }
 
+  get isNeedToExpand() {
+    const isButtonFocused = document.activeElement === this.buttonElement
+
+    return (!this.state.isExpanded && isButtonFocused)
+  }
+
+  selectedCurrentOption() {
+    this.state.selectedOptionElement = this.optionElements[this.state.currentOptionIndex]
+  }
+
   onButtonClick = () => {
     this.toggleExpandedState();
+  }
+
+  onClick = (event) => {
+    const { target } = event
+    const isOutsideDropdownClick = target.closest(this.selectors.dropdown) !== this.dropdownElement
+    const isButtonClick = target === this.buttonElement
+
+    if (!isButtonClick && isOutsideDropdownClick) {
+      this.collapse()
+      return
+    }
+
+    const isOptionClick = target.matches(this.selectors.option)
+
+    if (isOptionClick) {
+      this.state.selectedOptionElement = target
+      this.state.currentOptionIndex = [...this.optionElements]
+        .findIndex((optionElement) => optionElement === target)
+      this.collapse()
+    }
+  }
+
+  onArrowUpKeyDown = () => {
+    if (this.isNeedToExpand) {
+      this.expand()
+      return
+    }
+
+    if (this.state.currentOptionIndex > 0) {
+      this.state.currentOptionIndex--
+    }
+  }
+
+  onArrowDownKeyDown = () => {
+    if (this.isNeedToExpand) {
+      this.expand()
+      return
+    }
+
+    if (this.state.currentOptionIndex < this.optionElements.length - 1) {
+      this.state.currentOptionIndex++
+    }
+  }
+
+  onSpaceKeyDown = () => {
+    if (this.isNeedToExpand) {
+      this.expand()
+      return
+    }
+
+    this.selectedCurrentOption()
+    this.collapse()
+  }
+
+  onEnterKeyDown = () => {
+    if (this.isNeedToExpand) {
+      this.expand()
+      return
+    }
+
+    this.selectedCurrentOption()
+    this.collapse()
+  }
+
+  onKeyDown = (event) => {
+    const { code } = event
+
+    const action = {
+      ArrowUp: this.onArrowUpKeyDown,
+      ArrowDown: this.onArrowDownKeyDown,
+      Space: this.onSpaceKeyDown,
+      Enter: this.onEnterKeyDown,
+    }[code]
+
+    if (action) {
+      event.preventDefault()
+      action()
+    }
   }
 
   onMobileMatchMediaChange = (event) => {
     this.updateTabIndexes(event.matches)
   }
 
+  onOriginalControlChange = () => {
+    this.state.selectedOptionElement = this.optionElements[this.originalControlElement.selectedIndex]
+  }
+
   bindEvents() {
     MatchMedia.mobile.addEventListener('change', this.onMobileMatchMediaChange)
     this.buttonElement.addEventListener('click', this.onButtonClick)
+    document.addEventListener('click', this.onClick)
+    this.rootElement.addEventListener('keydown', this.onKeyDown)
+    this.originalControlElement.addEventListener('change', this.onOriginalControlChange)
   }
 }
 
